5b5fde625ca8cc67fbb5f855ab02d925a396699d
Yoga3911 <yoga3911@gmail.com>
2021-03-27 18:07:33 +0700

    Revert "blu_schedutil: tune up&down_rate_limit_us like schedutil"
    This reverts commit 52e1e434461d52cde351f7265c24d17f2a325e19.

--- kernel/sched/cpufreq_blu_schedutil.c
+++ kernel/sched/cpufreq_blu_schedutil.c
@@ -907,8 +907,10 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us = LATENCY_MULTIPLIER / 1;
-	tunables->down_rate_limit_us = LATENCY_MULTIPLIER * 4;
+	tunables->up_rate_limit_us = LATENCY_MULTIPLIER;
+	tunables->down_rate_limit_us = LATENCY_MULTIPLIER;
+	tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
+	tunables->hispeed_freq = 0;
 	lat = policy->cpuinfo.transition_latency / NSEC_PER_USEC;
 	if (lat) {
 		tunables->up_rate_limit_us *= lat;
