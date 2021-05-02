e429c8ce3ccdf9039b4b03e68c264d808f93ef57
Yoga3911 <yoga3911@gmail.com>
2021-05-02 20:18:30 +0700

    fix error

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -730,11 +730,6 @@ static int sugov_init(struct cpufreq_policy *policy)
 	}
 
 		tunables->iowait_boost_enable = false;
-	
-	tunables->up_rate_limit_us =
-				CONFIG_SCHEDUTIL_UP_RATE_LIMIT;
-	tunables->down_rate_limit_us =
-				CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
